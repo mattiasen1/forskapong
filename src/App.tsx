9b43309f708dd7a80678a21df1f259ac74c6a1aa
Sven-Erik Bertsson <mattiasen2@gmail.com>
2024-01-16 22:36:22 +0100

    New stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
+import { Bracket, IRoundProps, Seed, SingleLineSeed, ISingleEliminationProps, SeedItem, SeedTeam, IRenderSeedProps } from 'react-brackets';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const rounds: IRoundProps[] = [
+  {
+    title: 'Grupp 1',
+    seeds: [
+      {
+        id: 1,
+        teams: [{ name: 'Team A' }, { name: 'Team B'}, { name: 'Team C'}, { name: 'Team D'},],
+      },
+      {
+        id: 2,
+        teams: [{ name: 'Team A' }, { name: 'Team B'}, { name: 'Team C'}, { name: 'Team D'},],
+      },
+    ],
+  },
+];
+const CustomSeed = ({seed, breakpoint, roundIndex, seedIndex}: IRenderSeedProps) => {
+
+
+
+const isLineConnector = rounds[roundIndex].seeds.length === rounds[roundIndex + 1]?.seeds.length;
+
+const Wrapper = isLineConnector ? SingleLineSeed : Seed;
+
+  // mobileBreakpoint is required to be passed down to a seed
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper mobileBreakpoint={breakpoint} style={{ backgroundColor: 'transparent'}}>
+      <SeedItem>
+        <div>
+          <SeedTeam style={{ color: 'green', fontWeight: 'bold', fontSize: 15}}>{seed.teams[0]?.name || 'NO TEAM '}</SeedTeam>
+          <SeedTeam style={{ color: 'red', fontWeight: 'bold', fontSize: 15}}> {seed.teams[1]?.name || 'NO TEAM '}</SeedTeam>
+        </div>
+      </SeedItem>
+    </Wrapper>
+    
   );
-}
+};
+
+
+const App = () => {
+  return <Bracket 
+    rounds={rounds}
+    // roundTitleComponent={(title: React.ReactNode, roundIndex: number) => {
+    //   return <div style={{ textAlign: 'center', color: 'black', fontWeight: 'bold' }}>{title}</div>;
+    // }}
+    renderSeedComponent={CustomSeed}
+  />;
+};
 
 export default App;
+
+
